1c53d580c65a97f4b6daf3be4b0f41b4f2d99bb3
byemidev <emilioarevalozuniga@gmail.com>
2024-12-05 09:47:23 +0100

    ACT1.2 Part1 #A #Intro2React Course Information, step 2 finished

--- part1/act1_1/src/App.jsx
+++ part1/act1_1/src/App.jsx
@@ -19,11 +19,12 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-  //Error rendering or passing props btweeen Content and Part components
+  
   return (
     <div>
-      <Part content={props.content[0]}/>
-      <Part content={props.content[1]}/>
+      <Part part={props.content[0].part} exercises = {props.content[0].exercises}/>
+      <Part part={props.content[1].part} exercises = {props.content[1].exercises}/>
+      <Part part={props.content[2].part} exercises = {props.content[2].exercises}/>
     </div>
   )
 }
